55ef4f462efd6d862a81760f9c96cf7b4a511856
Şafak Tamses <safak.tamses@semper.tech.com>
2022-12-02 14:15:31 +1100

    DesingPattern
    .....

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import AbstractFactoryPattern.AbstractFactory;
+import AbstractFactoryPattern.AbstractFactoryPatternDemo;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    AbstractFactoryPatternDemo abstractFactoryPatternDemo;
+
 }
---
+++ src/ProxyPattern/ProxyPatternDemo.java
@@ -0,0 +1,10 @@
+package ProxyPattern;
+
+public class ProxyPatternDemo {
+    public static void main(String[] args) {
+        Image image = new ProxyImage("test_10mb.jpg");
+        image.display();
+        System.out.println(" ");
+        image.display();
+    }
+}
